4c1200a37d77a8e8e37d913a6b686a0b2a70e2eb
赵旭明 <hhappyzxm@163.com>
2015-12-07 19:50:55 +0800

    1.0.0.0

---
+++ DocumentEncryption/DocumentEncryption/F_Main.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.PerformanceData;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocumentEncryption
+{
+    public partial class F_Main : Form
+    {
+        public F_Main()
+        {
+            InitializeComponent();
+        }
+
+        private DataTable GenerateDataTable()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("AccountName");
+            dt.Columns.Add("Website");
+            dt.Columns.Add("UserName");
+            dt.Columns.Add("Password");
+            dt.Columns.Add("TradePassword");
+            dt.Columns.Add("Comment");
+
+            return dt;
+        }
+
+        public class AccountViewModel
+        {
+            public string AccountName { get; set; }
+            public string WebSite { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public string TradePassword { get; set; }
+            public string Comment { get; set; }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (DataGridViewRow row in dgvAccount.Rows)
+                {
+                    if (row.Index == dgvAccount.Rows.Count - 1)
+                    {
+                        continue;
+                    }
+
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        sb.Append(cell.Value + (cell.ColumnIndex == row.Cells.Count - 1 ? string.Empty : "|"));
+                    }
+
+                    if (row.Index < dgvAccount.Rows.Count - 2)
+                    {
+                        sb.Append("$");
+                    }
+                }
+                
+
+                using (FileStream fs = (FileStream) saveFileDialog1.OpenFile())
+                {
+                    var bytes = Encoding.UTF8.GetBytes(sb.ToString());
+                    var estr = Convert.ToBase64String(bytes);
+                    var ebytes = Encoding.UTF8.GetBytes(estr);
+                    fs.Write(ebytes, 0, ebytes.Length);
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                using (StreamReader fileStream = new StreamReader(openFileDialog1.FileName))
+                {
+                    var str = fileStream.ReadToEnd();
+                    str = Encoding.UTF8.GetString(Convert.FromBase64String(str));
+
+                    var dt = GenerateDataTable();
+
+                    foreach (var strRow in str.Split('$'))
+                    {
+                        var newRow = dt.NewRow();
+
+                        var i = 0;
+                        foreach (var strColumn in strRow.Split('|'))
+                        {
+                            newRow[i] = strColumn;
+
+                            i++;
+                        }
+
+                        dt.Rows.Add(newRow);
+                    }
+
+                    dgvAccount.DataSource = dt;
+                }
+            }
+        }
+    }
+}
---
+++ DocumentEncryption/DocumentEncryption/F_Main.designer.cs
@@ -0,0 +1,156 @@
+﻿namespace DocumentEncryption
+{
+    partial class F_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAccount = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAccount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAccount
+            // 
+            this.dgvAccount.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAccount.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column2,
+            this.Column6});
+            this.dgvAccount.Location = new System.Drawing.Point(12, 41);
+            this.dgvAccount.Name = "dgvAccount";
+            this.dgvAccount.RowTemplate.Height = 23;
+            this.dgvAccount.Size = new System.Drawing.Size(914, 560);
+            this.dgvAccount.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(93, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "AccountName";
+            this.Column1.HeaderText = "Account Name";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 120;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "UserName";
+            this.Column3.HeaderText = "User Name";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "Password";
+            this.Column4.HeaderText = "Password";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "TradePassword";
+            this.Column5.HeaderText = "Trade Password";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 120;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Website";
+            this.Column2.HeaderText = "Website";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "Comment";
+            this.Column6.HeaderText = "Comment";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 300;
+            // 
+            // F_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 613);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dgvAccount);
+            this.Name = "F_Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAccount)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAccount;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
